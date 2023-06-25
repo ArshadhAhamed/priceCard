2a8f80ad2b17da2714ac08ab71eaafc9b4300d23
Arshadh Ahamed <arshadhahamed777@gmail.com>
2023-06-25 11:06:07 +0530

    sb-admin day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let data=[
+    {
+      plan:"free",
+      price:"0",
+      users:"single user",
+      isUsers:true,
+      storage:"5GB storage",
+      isStorage:true,
+      publicProjects:"unlimited Public Projects",
+      isPublicProjects:true,
+      communityAcces:"Community Access",
+      isCommunityAcces:true,
+      privateProjects:"Unlimited Private Projects",
+      isPrivateProjects:false,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:false,
+      subdomain:"Free Subdomain",
+      isSubdomain:false,
+      reports:"Monthly Status Reports",
+      isReports:false
+
+
+
+
+}
+,{
+  plan:"plus",
+  price:"9",
+  users:"5 Users",
+  isUsers:true,
+  storage:"50GB storage",
+      isStorage:true,
+      publicProjects:"unlimited Public Projects",
+      isPublicProjects:true,
+      communityAcces:"Community Access",
+      isCommunityAcces:true,
+      privateProjects:"Unlimited Private Projects",
+      isPrivateProjects:true,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:true,
+      subdomain:"Free Subdomain",
+      isSubdomain:true,
+      reports:"Monthly Status Reports",
+      isReports:false
+},{
+  plan:"pro",
+  price:"49",
+  users:"Unlimited  Users",
+  isUsers:true,
+      storage:"150GB storage",
+      isStorage:true,
+      publicProjects:"unlimited Public Projects",
+      isPublicProjects:true,
+      communityAcces:"Community Access",
+      isCommunityAcces:true,
+      privateProjects:"Unlimited Private Projects",
+      isPrivateProjects:true,
+      phoneSupport:"Dedicated Phone Support",
+      isPhoneSupport:true,
+      subdomain:"Unlimited Free Subdomain",
+      isSubdomain:true,
+      reports:"Monthly Status Reports",
+      isReports:true
+}
+  ]
+ 
+  return <>
+    <section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+      {/* <!-- Free Tier --> */}
+      {/* <Card  data={data[0]}/>
+      <Card data={data[1]}/>
+      <Card data={data[2]}/> */}
+      {
+      data.map((e,i)=>{
+        return <Card data={e} key={i}/>
+      })
+    }
     </div>
-  );
+  </div>
+</section>
+  
+    </>;
 }
 
 export default App;
